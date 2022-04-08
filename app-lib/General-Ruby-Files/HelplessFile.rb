635335dddc76cd52db04b0b1d8c457b9f7d90c83
SDBoahen <SDBoahen@gmail.com>
2022-04-08 09:50:17 -0400

    Just Added HelplessFile.rb Lolol

---
+++ app-lib/General-Ruby-Files/HelplessFile.rb
@@ -0,0 +1,37 @@
+puts "
+
+hep... :(
+    
+"
+
+
+answer = 1 + 1
+puts "
+#{answer}
+
+"
+
+
+puts answer
+
+puts " "
+
+ruby_array = [ 1, 2, 3 ]
+puts ruby_array
+
+
+puts "
+
+But I Can Still Do Math ;D  (i.e Computer)
+    
+"
+
+
+
+def puts_a_name
+    puts "Sam"
+end
+# def end
+# def-end your method
+
+puts_a_name
